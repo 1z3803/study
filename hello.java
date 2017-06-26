953a7019674a7a0f97661c76a99230fb75ef623f
kim <kim@naver.com>
2017-06-26 14:48:31 +0900

    added ',.gitignore' file

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	
+	public static void main(String[] args){
+
+		System.out.println("Hello World");
+		System.out.println("Hello");
+		System.out.println("Tell");
+		System.out.println("Tell her World");
+
+	}
+}
